7f449bf9ae6d9de6022266c9dca67bf95bf78c0f
Ki Hyun Park <kihyun1998@penta.co.kr>
2026-02-02 14:54:25 +0900

    add macos

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_ime
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterImePlugin.register(with: registry.registrar(forPlugin: "FlutterImePlugin"))
+}
